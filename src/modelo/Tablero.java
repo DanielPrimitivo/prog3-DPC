7430be8b1870e88f62ba9ccaf2c4c612d25c811d
root <root@clL04D-8>
2017-10-03 12:09:35 +0200

    Versión 2.0.1 - Modificación Constructor Tablero

--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -6,15 +6,14 @@ package modelo;
 
 import java.util.*;
 
-import modelo.EstadoCelda;
-
 public class Tablero {
 	
 	private HashMap<Coordenada,EstadoCelda> celdas;
 	
 	public Tablero(Coordenada dimensiones) {
-		for (int i = 0;i <= dimensiones.getX();i++) {
-			for (int j = 0;j <= dimensiones.getY();j++) {
+		celdas = new HashMap<Coordenada,EstadoCelda>();
+		for (int i = 0;i < dimensiones.getX();i++) {
+			for (int j = 0;j < dimensiones.getY();j++) {
 				Coordenada coordenada = new Coordenada(i,j);
 				celdas.put(coordenada, EstadoCelda.MUERTA);
 			}
